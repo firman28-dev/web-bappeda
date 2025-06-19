6d8a0e2ef6959f53357e4428680a3bfb72ea9105
Bappeda <bappeda@Bappedas-MacBook-Pro.local>
2025-06-19 18:07:43 +0700

    test4

--- resources/views/partials/error_page/master.blade.php
+++ resources/views/partials/error_page/master.blade.php
@@ -3,12 +3,12 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Cuba admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Cuba admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="description" content="Website BAPPEDA Sumatera Barat adalah website yang memperlihatkan informasi publik">
+    <meta name="keywords" content="website bappeda">
     <meta name="author" content="pixelstrap">
 	<link rel="icon" type="image/png" href="{{ asset('logo/B BAPPEDA.png') }}" sizes="32x32">
-    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/x-icon">
     <title>@yield('title') | Bappeda - Sumatera Barat</title>
     <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet">
